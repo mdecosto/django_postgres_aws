883f0e4f1c6dedf520f4e75db1a2559107e0a1f0
mdecosto <mdecosto@futurex.ph>
2023-01-11 10:09:05 +0800

    add templates

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customers')
+    return render(request, 'accounts/customer.html')
