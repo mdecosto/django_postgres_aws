1cf15d1e4c59fbaef1b870a8ae7d64c4f95201f1
mdecosto <mdecosto@futurex.ph>
2023-01-13 14:48:01 +0800

    create aws s3 modules

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -77,6 +77,15 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'DEMO_TEST',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'futurex123!@#',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
